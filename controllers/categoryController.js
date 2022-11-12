8457dd0848236aa69007e2d546ca894940a33ab6
sohamviradiya <sohamviradiya2003@gmail.com>
2022-11-12 15:58:37 +0530

    add categories list

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,33 +3,38 @@ const Product = require("../models/product");
 
 exports.categoryController = {
      list: (req, res) => {
-          res.send("Category List Not Yet Implemented")
+          Category.find().exec().then((categories) => {
+               res.render("category/list", { categories, title: "Categories" });
+          }).catch((err) => {
+               console.log(err);
+               res.render("error", { error: err });
+          });
      },
      detail: (req, res) => {
-          res.send("Category Details Not Yet Implemented")
+          res.send("Category Details Not Yet Implemented");
      },
      create: {
           get: (req, res) => {
-               res.send("Category Creation Get Not Yet Implemented")
+               res.send("Category Creation Get Not Yet Implemented");
           },
           post: (req, res) => {
-               res.send("Category Creation Post Not Yet Implemented")
+               res.send("Category Creation Post Not Yet Implemented");
           }
      },
      update: {
           get: (req, res) => {
-               res.send("Category Updation Get Not Yet Implemented")
+               res.send("Category Updation Get Not Yet Implemented");
           },
           post: (req, res) => {
-               res.send("Category Updation Post Not Yet Implemented")
+               res.send("Category Updation Post Not Yet Implemented");
           }
      },
      delete: {
           get: (req, res) => {
-               res.send("Category Deletion Get Not Yet Implemented")
+               res.send("Category Deletion Get Not Yet Implemented");
           },
           post: (req, res) => {
-               res.send("Category Deletion Post Not Yet Implemented")
+               res.send("Category Deletion Post Not Yet Implemented");
           }
      }
 };

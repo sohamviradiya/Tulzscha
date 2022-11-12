57019781b6b91d5516519bcbaa598b3f0498f247
sohamviradiya <sohamviradiya2003@gmail.com>
2022-11-12 15:58:25 +0530

    add items list

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -3,33 +3,35 @@ const Item = require("../models/item");
 
 exports.itemController = {
      list: (req, res) => {
-          res.send("Item List Not Yet Implemented")
+          Item.find().populate("product").exec().then((items) => {
+               res.render("item/list", { items, title: "Items" });
+          });
      },
      detail: (req, res) => {
-          res.send("Item Details Not Yet Implemented")
+          res.send("Item Details Not Yet Implemented");
      },
      create: {
           get: (req, res) => {
-               res.send("Item Creation Get Not Yet Implemented")
+               res.send("Item Creation Get Not Yet Implemented");
           },
           post: (req, res) => {
-               res.send("Item Creation Post Not Yet Implemented")
+               res.send("Item Creation Post Not Yet Implemented");
           }
      },
      update: {
           get: (req, res) => {
-               res.send("Item Updation Get Not Yet Implemented")
+               res.send("Item Updation Get Not Yet Implemented");
           },
           post: (req, res) => {
-               res.send("Item Updation Post Not Yet Implemented")
+               res.send("Item Updation Post Not Yet Implemented");
           }
      },
      delete: {
           get: (req, res) => {
-               res.send("Item Deletion Get Not Yet Implemented")
+               res.send("Item Deletion Get Not Yet Implemented");
           },
           post: (req, res) => {
-               res.send("Item Deletion Post Not Yet Implemented")
+               res.send("Item Deletion Post Not Yet Implemented");
           }
      }
 };

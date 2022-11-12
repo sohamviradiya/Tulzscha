7fc9bf87dd1e3b87e19818c9a4d50fccc812f6be
sohamviradiya <sohamviradiya2003@gmail.com>
2022-11-12 15:58:53 +0530

    add categories list

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,36 +1,38 @@
 const Category = require("../models/category");
 const Brand = require("../models/brand");
 const Product = require("../models/product");
-
+const { body, validationResult } = require("express-validator");
 exports.productController = {
      list: (req, res) => {
-          res.send("Product List Not Yet Implemented")
+          Product.find({}, "name image price brand").sort({"name": 1}).populate("brand").exec().then((products) => {
+               res.render("product/list", { title: "Product List", products: products });
+          });
      },
      detail: (req, res) => {
-          res.send("Product Details Not Yet Implemented")
+          res.send("Product Details Not Yet Implemented");
      },
-     create: {
-          get: (req, res) => {
-               res.send("Product Creation Get Not Yet Implemented")
-          },
-          post: (req, res) => {
-               res.send("Product Creation Post Not Yet Implemented")
-          }
+          create: {
+     get: (req, res) => {
+          res.send("Product Creation Get Not Yet Implemented");
      },
-     update: {
-          get: (req, res) => {
-               res.send("Product Updation Get Not Yet Implemented")
-          },
           post: (req, res) => {
-               res.send("Product Updation Post Not Yet Implemented")
+               res.send("Product Creation Post Not Yet Implemented");
           }
+},
+update: {
+     get: (req, res) => {
+          res.send("Product Updation Get Not Yet Implemented");
      },
-     delete: {
-          get: (req, res) => {
-               res.send("Product Deletion Get Not Yet Implemented")
-          },
           post: (req, res) => {
-               res.send("Product Deletion Post Not Yet Implemented")
+               res.send("Product Updation Post Not Yet Implemented");
           }
-     }
+},
+delete: {
+     get: (req, res) => {
+          res.send("Product Deletion Get Not Yet Implemented");
+     },
+          post: (req, res) => {
+               res.send("Product Deletion Post Not Yet Implemented");
+          }
+}
 };

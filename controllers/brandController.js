ff0d2a67e9b3c5288baae405e4872fe9d740abb3
sohamviradiya <sohamviradiya2003@gmail.com>
2022-11-12 15:58:09 +0530

    add brands list

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -3,33 +3,38 @@ const Product = require("../models/product");
 
 exports.brandController = {
      list: (req, res) => {
-          res.send("Brand List Not Yet Implemented")
+          Brand.find().exec().then((brands) => {
+               res.render("brand/list", {brands, title: "Brands"});
+          }).catch((err) => {
+               console.log(err);
+               res.render("error", { error: err });
+          });
      },
      detail: (req, res) => {
-          res.send("Brand Details Not Yet Implemented")
+          res.send("Brand Details Not Yet Implemented");
      },
      create: {
           get: (req, res) => {
-               res.send("Brand Creation Get Not Yet Implemented")
+               res.send("Brand Creation Get Not Yet Implemented");
           },
           post: (req, res) => {
-               res.send("Brand Creation Post Not Yet Implemented")
+               res.send("Brand Creation Post Not Yet Implemented");
           }
      },
      update: {
           get: (req, res) => {
-               res.send("Brand Updation Get Not Yet Implemented")
+               res.send("Brand Updation Get Not Yet Implemented");
           },
           post: (req, res) => {
-               res.send("Brand Updation Post Not Yet Implemented")
+               res.send("Brand Updation Post Not Yet Implemented");
           }
      },
      delete: {
           get: (req, res) => {
-               res.send("Brand Deletion Get Not Yet Implemented")
+               res.send("Brand Deletion Get Not Yet Implemented");
           },
           post: (req, res) => {
-               res.send("Brand Deletion Post Not Yet Implemented")
+               res.send("Brand Deletion Post Not Yet Implemented");
           }
      }
 };

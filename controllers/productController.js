d7adbee4342783eb61dd94d968ebc62d26b51046
sohamviradiya <sohamviradiya2003@gmail.com>
2022-11-13 14:21:19 +0530

    add product updation and deletion feature

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,10 @@
 const Product = require("../models/product");
 const Item = require("../models/item");
+const Category = require("../models/category");
+const Brand = require("../models/brand");
+
 const { body, validationResult } = require("express-validator");
+const category = require("../models/category");
 exports.productController = {
      list: (req, res) => {
           Product.find({}, "name image price brand").sort({ "name": 1 }).populate("brand").exec().then((products) => {
@@ -10,31 +14,104 @@ exports.productController = {
      detail: (req, res) => {
           const id = req.params.id;
           Promise.all([Product.findById(id).populate("brand").populate("category").exec(),
-               Item.find({ product: id }).exec()]).then(([product, items]) => {
-                    if (!product)
-                         res.render("error", { error: "Product not found" });
-                    else if (!items)
-                         res.render("error", { error: "Items not found" });
-                    else
-                         res.render("product/detail", { product: product, items: items, title: product.name });
-               }).catch((err) => {
-                    res.render("error", { error: err });
-               });
+          Item.find({ product: id }).exec()]).then(([product, items]) => {
+               if (!product)
+                    res.render("error", { error: "Product not found" });
+               else if (!items)
+                    res.render("error", { error: "Items not found" });
+               else
+                    res.render("product/detail", { product: product, items: items, title: product.name });
+          }).catch((err) => {
+               res.render("error", { error: err });
+          });
      },
      create: {
           get: (req, res) => {
-               res.send("Product Creation Get Not Yet Implemented");
+               Promise.all([
+                    Category.find({}).exec(),
+                    Brand.find({}).exec()]
+               ).then(([categories, brands]) => {
+                    res.render("product/create", { title: "Create Product", categories: categories, brands: brands });
+               }).catch((err) => {
+                    res.render("error", { error: err });
+               });
           },
           post: (req, res) => {
-               res.send("Product Creation Post Not Yet Implemented");
+               if (req.body.category_selection) {
+                    Promise.all([
+                         Category.findById(req.body.category).exec(),
+                         Brand.findById(req.body.brand).exec()
+                    ]).then(([category, brand]) => {
+                         res.render("product/create", { title: "Create Product", category: category, brand: brand });
+                    }).catch((err) => {
+                         res.render("error", { error: err });
+                    });
+               } else {
+                    Category.findById(req.body.category).exec().then((category) => {
+                         const stats = new Map();
+                         category.statfields.forEach((field) => {
+                              stats.set(field, req.body[field]);
+                         });
+                         console.log(stats);
+                         const product = new Product({
+                              name: req.body.name,
+                              description: req.body.description,
+                              price: req.body.price,
+                              image: req.body.image,
+                              brand: req.body.brand,
+                              category: req.body.category,
+                              warrenty: req.body.warrenty,
+                              stats: stats,
+                         });
+                         product.save().then((product) => {
+                              res.redirect(`/catalog/product/${product._id}`);
+                         }).catch((err) => {
+                              res.render("error", { error: err });
+                         });
+                    }).catch((err) => {
+                         res.render("error", { error: err });
+                    });
+               }
           }
      },
      update: {
           get: (req, res) => {
-               res.send("Product Updation Get Not Yet Implemented");
+               const id = req.params.id;
+               Product.findById(id).populate("brand").populate("category").exec().then(
+                    (product) => {
+                         console.log(product.stats);
+                         if (!product)
+                              res.render("error", { error: "Product not found" });
+                         else
+                              res.render("product/create", { title: "Update Product", product: product, category: product.category, brand: product.brand });
+                    }).catch((err) => {
+                         res.render("error", { error: err });
+                    });
           },
           post: (req, res) => {
-               res.send("Product Updation Post Not Yet Implemented");
+               Category.findById(req.body.category).exec().then((category) => {
+                    const stats = new Map();
+                    category.statfields.forEach((field) => {
+                         stats.set(field, req.body[field]);
+                    });
+                    console.log(stats);
+                    const product = new Product({
+                         name: req.body.name,
+                         description: req.body.description,
+                         price: req.body.price,
+                         image: req.body.image,
+                         brand: req.body.brand,
+                         category: req.body.category,
+                         warrenty: req.body.warrenty,
+                         stats: stats,
+                         _id: req.params.id,
+                    });
+                    Product.findByIdAndUpdate(req.params.id, product, {}).then((product) => {
+                         res.redirect(`/catalog/product/${product._id}`);
+                    });
+               }).catch((err) => {
+                    res.render("error", { error: err });
+               });
           }
      },
      delete: {
